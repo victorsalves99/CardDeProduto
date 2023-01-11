21a5af0b7dc4fb018adc9df692b6a0ef1d012e65
victorsalves99 <victorsalves99@outlook.com>
2023-01-11 18:28:39 -0300

    upgrade

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
 const img360 = document.getElementById("img360")
 const  imagSofa = document.getElementById("sofa")
+
 img360.addEventListener("click",() => {
-    console.log(imagSofa.dataset.name );
     if (imagSofa.dataset.name === "static") {
         imagSofa.src = "image/sofa360.gif"
         img360.src = "image/x.png"
         console.log("mudou");
         imagSofa.dataset.name = "mobile"
-        
     }else {
         imagSofa.src = "image/sofa.png"
         img360.src = "image/360.png"
